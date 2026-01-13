14d621f1414ef92844fafb3458cd04789132b347
Sebastien_besse <sebastien_besse@yahoo.com>
2026-01-13 16:16:46 +0100

    Add pictures & Created Authentification View

---
+++ app/src/main/java/com/example/todoapp/View/AuthView.kt
@@ -0,0 +1,106 @@
+package com.example.todoapp.View
+
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.todoapp.ui.theme.Background
+import com.example.todoapp.ui.theme.PrimaryPurple
+import com.example.todoapp.ui.theme.TodoAppTheme
+
+@Composable
+fun LoginScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Background), // fond gris clair
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier
+                .padding(16.dp)
+                .padding(24.dp)
+        ) {
+            Text(
+                text = "Login",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+
+            Spacer(modifier = Modifier.height(200.dp))
+
+            Text(
+                text = "Username",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.White
+            )
+            // Password field
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Enter your Username") },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Password",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.White
+            )
+            // Password field
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text("••••••••••",
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                        },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            Spacer(modifier = Modifier.height(80.dp))
+
+            // Login button
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = PrimaryPurple
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                shape = RoundedCornerShape(3.dp)
+            ) {
+                Text(text = "Login", fontSize = 16.sp)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AuthPreview() {
+    TodoAppTheme {
+        LoginScreen()
+    }
+}
--- app/src/main/java/com/example/todoapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/todoapp/ui/theme/Color.kt
@@ -10,6 +10,8 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val Background = Color(0xFF121212)
+
 val PrimaryPurple = Color(0xFF8687E7)
 
 val  GreenCategoryBackground = Color(0xFFCCFF80)
